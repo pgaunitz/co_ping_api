9becbcba1a3a2c9927a21c79556d8420690179f3
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-12 12:19:14 +0200

    testing Rspec, changing file path to environment, creating Json Module.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
 
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require File.expand_path('../../config/environment', __FILE__)
+
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
 require 'spec_helper'
+require 'rspec/rails'
 
 
-  ActiveRecord::Migration.maintain_test_schema!
+ActiveRecord::Migration.maintain_test_schema!
 
-  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -17,7 +21,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-  config.include ResponseJson
+  config.include ResponseJSON
 end
 FactoryBot::SyntaxRunner.class_eval do
   include ActionDispatch::TestProcess
