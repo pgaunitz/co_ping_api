c8dfb7ea2d6f711e4e56dec4e728879e34e6ae43
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-13 17:15:07 +0200

    updating test for creating ping  with sad path

--- spec/requests/user_can_create_ping_spec.rb
+++ spec/requests/user_can_create_ping_spec.rb
@@ -1,62 +1,70 @@
 # frozen_string_literal: true
 
 RSpec.describe 'post /pings', type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
-
-  describe 'user POST a ping' do
-    before do
-      post '/pings',
-           params: {
-             ping: {
-               time: '2020-04-31-15:00',
-               store: 'ica'
-             }
-           },
-           headers: headers
+  describe 'with valid credentials' do
+    let(:user) { create(:user, role: 'user') }
+    let(:user_credentials) { user.create_new_auth_token }
+    let(:user_headers) do
+      { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials)
     end
 
-    it 'returns a 200 response status' do
-      expect(response).to have_http_status 200
+    describe 'user POST a ping' do
+      before do
+        post '/pings',
+             params: { ping: { time: '2020-04-31-15:00', store: 'ica' } },
+             headers: user_headers
+      end
+
+      it 'returns a 200 response status' do
+        expect(response).to have_http_status 200
+      end
+      it 'returns success message' do
+        expect(response_json['message']).to eq 'Your new trip is now active'
+      end
     end
-    it 'returns success message' do
-      expect(response_json['message']).to eq 'Your new trip is now active'
+
+    describe 'user POST a ping without store' do
+      before do
+        post '/pings',
+             params: { ping: { time: '2020-04-31-15:00' } },
+             headers: user_headers
+      end
+
+      it 'returns a 200 response status' do
+        expect(response).to have_http_status 200
+      end
+      it 'returns success message' do
+        expect(response_json['message']).to eq 'Your new trip is now active'
+      end
+    end
+
+    describe 'user POST a ping without time' do
+      before do
+        post '/pings', params: { ping: { store: 'Ica' } }, headers: user_headers
+      end
+      it 'returns a 422 response status' do
+        expect(response).to have_http_status 422
+      end
+      it 'returns error message' do
+        expect(
+          response_json['message']
+        ).to eq 'You need to set a time for your new trip'
+      end
     end
   end
 
-  describe 'user POST a ping without store' do
+  describe 'without valid credentials' do
+    let(:headers) { { HTTP_ACCEPT: 'application/json' } }
     before do
       post '/pings',
-           params: {
-             ping: {
-               time: '2020-04-31-15:00'
-             }
-           },
+           params: { ping: { time: '2020-04-31-15:00', store: 'ica' } },
            headers: headers
     end
-
-    it 'returns a 200 response status' do
-      expect(response).to have_http_status 200
+    it 'returns a 401 response status' do
+      expect(response).to have_http_status 401
     end
     it 'returns success message' do
-      expect(response_json['message']).to eq 'Your new trip is now active'
-    end
-  end
-
-  describe 'user POST a ping without time' do
-    before do
-      post '/pings',
-           params: {
-             ping: {
-               store: 'Ica'
-             }
-           },
-           headers: headers
-    end
-    it 'returns a 422 response status' do
-      expect(response).to have_http_status 422
-    end
-    it 'returns error message' do
-      expect(response_json['message']).to eq 'You need to set a time for your new trip'
+      expect(response_json['errors'].first).to eq "You need to sign in or sign up before continuing."
     end
   end
 end
