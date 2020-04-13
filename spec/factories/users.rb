2491aa160c9c8934ae20011b2d45235af045cf72
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-13 16:59:05 +0200

    adding test for successful user login and authenticating user before actions in pings controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :user do
-    role { "admin" }
+    role { "user" }
     email { "admin@mail.com" }
     password { "password" }
     password_confirmation { "password" }
     name { "Betty" }
+  end
+  factory :admin do
+    role { 'admin' }
   end
 end
