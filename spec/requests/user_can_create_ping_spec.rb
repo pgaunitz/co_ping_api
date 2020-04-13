f4457c787cf068effb76c274243f1478daaf4023
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-13 11:01:08 +0200

    adds initail test for user can create ping, adds route

---
+++ spec/requests/user_can_create_ping_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe 'post /pings', type: :request do
+  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+  describe 'user POST a ping' do
+    before do
+      post '/pings',
+        params: {
+          time: "2020-04-31-15:00",
+          store: "ica"
+        },
+        headers: headers
+    end
+
+    it 'returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+    it 'returns success message' do
+      expect(resonse_json['message']).to eq "Your trip is now active"
+    end
+  end
+end
