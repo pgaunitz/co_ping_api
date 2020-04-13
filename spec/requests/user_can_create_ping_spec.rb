2dfabe7d6de2aed8ab2a9e9dc261b64f29fea8a3
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-13 11:32:48 +0200

    adding sad path test

--- spec/requests/user_can_create_ping_spec.rb
+++ spec/requests/user_can_create_ping_spec.rb
@@ -33,12 +33,30 @@ RSpec.describe 'post /pings', type: :request do
            },
            headers: headers
     end
-    
+
     it 'returns a 200 response status' do
       expect(response).to have_http_status 200
     end
     it 'returns success message' do
       expect(response_json['message']).to eq 'Your new trip is now active'
+    end
+  end
+
+  describe 'user POST a ping without time' do
+    before do
+      post '/pings',
+           params: {
+             ping: {
+               store: 'Ica'
+             }
+           },
+           headers: headers
+    end
+    it 'returns a 422 response status' do
+      expect(response).to have_http_status 422
+    end
+    it 'returns success message' do
+      expect(response_json['message']).to eq 'You need to set a time for your new trip'
     end
   end
 end
