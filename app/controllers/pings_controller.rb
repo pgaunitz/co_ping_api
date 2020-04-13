fe33e068dd3717c04575e66366d3558e467cea87
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-13 12:16:03 +0200

    formatting

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -2,17 +2,16 @@ class PingsController < ApplicationController
   def create
     ping = Ping.create(ping_params)
     if ping.persisted?
-      render json: { message: 'Your new trip is now active'}
+      render json: { message: 'Your new trip is now active' }
     else
-      render json: { message: 'You need to set a time for your new trip'},
-      status: 422
+      render json: { message: 'You need to set a time for your new trip' },
+             status: 422
     end
-    
   end
 
   private
 
   def ping_params
-      params.require(:ping).permit(:time, :store)
+    params.require(:ping).permit(:time, :store)
   end
 end
