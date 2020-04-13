672d0f98a571fe13520b176dedd9727f3ed0c86b
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-13 18:05:25 +0200

    seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
 User.create email: 'admin@mail.com', password: 'password', role: 'admin'
+User.create email: 'user@mail.com', password: 'password', role: 'user'
+Ping.create time: '2020-06-30 15:00', store: 'Systembolaget', active: true
+Ping.create time: '2020-05-22 18:00', store: 'ICA', active: true
