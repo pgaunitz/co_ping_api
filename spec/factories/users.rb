c4b0b36953e6fe089f8f83e0bfb42ed9cb604b3b
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-13 18:10:40 +0200

    formatting f-bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    role { "user" }
-    email { "admin@mail.com" }
-    password { "password" }
-    password_confirmation { "password" }
-    name { "Betty" }
+    role { 'user' }
+    email { 'admin@mail.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+    name { 'Betty' }
   end
   factory :admin do
     role { 'admin' }
