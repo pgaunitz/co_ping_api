ba96cb037e6327f13eb086a9731e2c35436b9581
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-14 17:58:08 +0200

    uncomments and empties the service

--- app/services/pong_service.rb
+++ app/services/pong_service.rb
@@ -1,14 +1,3 @@
-# module PongService
-#   def self.pong_creator(pong_params)
-#     pong = Pong.create(pong_params)
-#     if pong.persisted?
-#       render json: { message: 'Your request was added to this trip' }
-#     else
-#       error_handler('You have to specify what items you need')
-#     end
-#   end
+module PongService
 
-#   def self.error_handler(error)
-#     render json: { message: error }, status: 422
-#   end
-# end
+end
