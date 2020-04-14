48f2c21e1ec6fccfe43f455b35750afd9db579ee
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-14 15:19:47 +0200

    adds test that user cannot create pong to an inactive ping adds restriction to pongscontroller

--- app/controllers/pongs_controller.rb
+++ app/controllers/pongs_controller.rb
@@ -1,13 +1,18 @@
 class PongsController < ApplicationController
   before_action :authenticate_user!
   def create
-    pong = Pong.create(pong_params)
-    render json: { message: 'Your request was added to this trip'}
+    if Ping.all.find(pong_params['ping_id']).active
+      pong = Pong.create(pong_params)
+      render json: { message: 'Your request was added to this trip' }
+    else
+      render json: { message: 'This trip is no longer active' },
+      status: 422
+    end
   end
 
-  private 
+  private
 
   def pong_params
-    params.require(:pong).permit(:item1, :item2, :item3, :user, :ping)
+    params.require(:pong).permit(:item1, :item2, :item3, :user_id, :ping_id)
   end
 end
--- spec/requests/user_can_create_pong_spec.rb
+++ spec/requests/user_can_create_pong_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe 'POST /pongs', type: :request do
-  describe 'successfully' do
-    let(:user) { create(:user, role: 'user')}
-    let(:ping) { create(:ping, user: user) }
+  describe 'with valid credentials' do
+    
     let(:user) { create(:user, role: 'user') }
     let(:user_credentials) { user.create_new_auth_token }
     let(:user_headers) do
@@ -11,6 +10,7 @@ RSpec.describe 'POST /pongs', type: :request do
     end
     
     describe 'can create pong' do
+      let(:ping) { create(:ping) }
       before do
         post '/pongs',
              params:
@@ -29,9 +29,69 @@ RSpec.describe 'POST /pongs', type: :request do
         expect(response.status).to eq 200
       end
 
-      it 'returns success message' do
+      it 'successfully creates pong' do
         expect(response_json['message']).to eq 'Your request was added to this trip'
       end
+
+      it 'pong belongs to ping' do
+        expect(Pong.last.ping_id).to eq ping.id
+      end
     end
+
+    describe 'can create pong with one item' do
+      let(:ping) { create(:ping) }
+      before do
+        post '/pongs',
+             params:
+             { pong:
+              {
+                item1: 'Bacon',
+                item2: nil,
+                item3: nil,
+                ping_id: ping.id,
+                user_id: user.id
+              } },
+             headers: user_headers
+      end
+
+      it 'returns response status 200' do
+        expect(response.status).to eq 200
+      end
+
+      it 'successfully creates pong' do
+        expect(response_json['message']).to eq 'Your request was added to this trip'
+      end
+
+      it 'pong belongs to ping' do
+        expect(Pong.last.ping_id).to eq ping.id
+      end
+    end
+
+    describe 'cannot create pong to inactive ping' do
+      let(:ping) { create(:ping, active: false) }
+      before do
+        post '/pongs',
+             params:
+             { pong:
+              {
+                item1: 'Bacon',
+                item2: nil,
+                item3: nil,
+                ping_id: ping.id,
+                user_id: user.id
+              } },
+             headers: user_headers
+      end
+
+      it 'returns response status 422' do
+        expect(response.status).to eq 422
+      end
+
+      it 'cannot create pong to inactive ping' do
+        expect(response_json['message']).to eq 'This trip is no longer active'
+      end
+    end
+
+  
   end
 end
