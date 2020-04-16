def4b28ade5504a63c786fa759cdcb9a3cc0e3f0
Philip Gaunitz <fluffen@pgaunitz.se>
2020-04-16 22:25:26 +0200

    adds test that only admn can change community status, adds restriction in community controller, adds update action to update users community status
    Co-authored-by: johan <johan@waytogo.se>

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,6 +2,7 @@
 
 class CommunitiesController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_user_admin, only: :update
   def index
     id = Community.all.select { |community| community.code == params['q'] }.first.id
     render json: { community_id: id }
@@ -10,6 +11,34 @@ class CommunitiesController < ApplicationController
   end
 
   def update
-    render json: { message: 'User is now accepted to your community.' }
+    if accept_user? == 'accepted'
+      update_user_community_status
+      render json: { message: 'User is now accepted to your community' }
+    else
+      update_user_community_status
+      render json: { message: 'User is now rejected from your community' }
+  end
+end
+
+  private
+
+  def user_admission
+    params.require(:user_admission).permit(:community_id, :community_status, :user_id )
+  end
+
+  def accept_user?
+    user_admission['community_status']
+  end
+
+  def update_user_community_status
+    user = User.all.find(user_admission['user_id']).update(community_status: accept_user? )
+  end
+
+  def is_user_admin
+    if current_user.role == "admin"
+    else
+      render json: { message: 'You are not authorized to do this, ask your admin' },
+      status: 401
+    end
   end
 end
--- spec/requests/admin_can_accept_or_reject_user_spec.rb
+++ spec/requests/admin_can_accept_or_reject_user_spec.rb
@@ -1,4 +1,5 @@
 RSpec.describe 'PUT /communities', type: :request do
+  let(:community) { create(:community) }
   let(:user) { create(:user, role: 'user', community_status: 'pending') }
     let(:user_credentials) { user.create_new_auth_token }
     let(:user_headers) do
@@ -9,12 +10,12 @@ RSpec.describe 'PUT /communities', type: :request do
     let(:admin_headers) do
       { HTTP_ACCEPT: 'application/json' }.merge!(admin_credentials)
     end
-    describe 'admin can accept or reject user to community' do
-      let(:community) { create(:community) } 
+    describe 'admin can accept user to community' do
+       
       before do
         put "/communities/#{community.id}",
              params: {
-               user: {
+               user_admission: {
                  community_id: community.id,
                  community_status: 'accepted',
                  user_id: user.id
@@ -28,7 +29,59 @@ RSpec.describe 'PUT /communities', type: :request do
       end
       
       it 'returns success message' do
-        expect(response_json['message']).to eq 'User is now accepted to your community.'
+        expect(response_json['message']).to eq 'User is now accepted to your community'
       end
+
+      it 'admin can accept user admission' do
+        expect(User.all.find(user.id).community_status).to eq 'accepted'
+      end
+
+      
+    end
+
+    describe 'admin can reject user to community' do
+      before do
+        put "/communities/#{community.id}",
+             params: {
+               user_admission: {
+                 community_id: community.id,
+                 community_status: 'rejected',
+                 user_id: user.id
+               }
+             },
+             headers: admin_headers
+      end
+
+      it 'returns success message' do
+        expect(response_json['message']).to eq 'User is now rejected from your community'
+      end
+
+      it 'admin can reject user admission' do
+        expect(User.all.find(user.id).community_status).to eq 'rejected'
+      end
+    end
+
+    describe 'only admin can change community status' do
+      before do
+        put "/communities/#{community.id}",
+             params: {
+               user_admission: {
+                 community_id: community.id,
+                 community_status: 'rejected',
+                 user_id: user.id
+               }
+             },
+             headers: user_headers
+      end
+
+      it 'returns a 401 response status' do
+        expect(response).to have_http_status 401
+      end
+
+      it 'returns error message' do
+        expect(response_json['message']).to eq 'You are not authorized to do this, ask your admin'
+      end
+
+     
     end
 end
