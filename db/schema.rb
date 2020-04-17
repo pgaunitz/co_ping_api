60ca6e7dfd8aac4f3ae39087107c1d87f2aa7b7b
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-17 19:07:44 +0200

    adding attribute status to pongs and updating seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_090814) do
+ActiveRecord::Schema.define(version: 2020_04_17_170149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_090814) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: true
+    t.integer "status", default: 0
     t.index ["ping_id"], name: "index_pongs_on_ping_id"
     t.index ["user_id"], name: "index_pongs_on_user_id"
   end
